c500f64caa770a1891c5ff00b8eaacf0cfc3152b
hungnt <hungnt@gmail.com>
2023-06-03 23:17:17 +0700

    update router

--- gateway/index.js
+++ gateway/index.js
@@ -6,9 +6,9 @@ const proxy = require('express-http-proxy')
 app.use(cors())
 app.use(express.json())
 
-app.use('/', proxy('http://localhost:8002'));
 app.use('/customer', proxy('http://localhost:8001'));
 app.use('/shopping', proxy('http://localhost:8003'));
+app.use('/', proxy('http://localhost:8002'));
 
 app.listen(8000, () => {
     console.log('app listern port 8000')
